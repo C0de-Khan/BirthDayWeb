8012434910006c88e643d8855d5989511398da88
CodeSades <522h0030@student.tdtu.edu.vn>
2024-06-16 10:23:00 +0700

    16/6/2024

---
+++ js/option.js
@@ -0,0 +1,11 @@
+var song1 = document.getElementById('song1');
+song1.volume = 0.4;
+
+var song2 = document.getElementById('song2');
+song2.volume = 0.4;
+
+var song3 = document.getElementById('song3');
+song3.volume = 0.4;
+
+var song4 = document.getElementById('song4');
+song4.volume = 0.4;
